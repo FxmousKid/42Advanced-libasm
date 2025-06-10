6aa0ef16a05387583c843838874cdd5099c9035d
Iyan Nazarian <36068362+FxmousKid@users.noreply.github.com>
2025-06-10 22:27:49 +0300

    chore: adding some test files

---
+++ src/ft_strlen.s
@@ -0,0 +1,6 @@
+global ft_return_42
+
+section .text
+ft_return_42:
+	mov rax, 43
+	ret
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+extern int ft_return_42(void);
+
+int main(void)
+{
+	printf("Returned value = %d\n", ft_return_42());
+	return (0);
+}
